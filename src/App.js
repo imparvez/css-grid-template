4157d615e78936e6067573f917c9cd173ed1595d
Parvez Shaikh <parvezshaikhj@gmail.com>
2019-02-02 23:34:44 +0530

    Grid Template Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,269 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className='grid-1 gutter-1'>
+          <div className='card'>
+            <p>1</p>
+          </div>
+        </div>
+        <div className='grid-2 gutter-1'>
+          <div className='card'>
+            <p>1</p>
+          </div>
+          <div className='card'>
+            <p>2</p>
+          </div>
+        </div>
+        <div className='grid-3 gutter-1'>
+          <div className='card'>
+            <p>1</p>
+          </div>
+          <div className='card'>
+            <p>2</p>
+          </div>
+          <div className='card'>
+            <p>3</p>
+          </div>
+        </div>
+        <div className='grid-4 gutter-1'>
+          <div className='card'>
+            <p>1</p>
+          </div>
+          <div className='card'>
+            <p>2</p>
+          </div>
+          <div className='card'>
+            <p>3</p>
+          </div>
+          <div className='card'>
+            <p>4</p>
+          </div>
+        </div>
+        <div className='grid-5 gutter-1'>
+          <div className='card'>
+            <p>1</p>
+          </div>
+          <div className='card'>
+            <p>2</p>
+          </div>
+          <div className='card'>
+            <p>3</p>
+          </div>
+          <div className='card'>
+            <p>4</p>
+          </div>
+          <div className='card'>
+            <p>5</p>
+          </div>
+        </div>
+        <div className='grid-6 gutter-1'>
+          <div className='card'>
+            <p>1</p>
+          </div>
+          <div className='card'>
+            <p>2</p>
+          </div>
+          <div className='card'>
+            <p>3</p>
+          </div>
+          <div className='card'>
+            <p>4</p>
+          </div>
+          <div className='card'>
+            <p>5</p>
+          </div>
+          <div className='card'>
+            <p>6</p>
+          </div>
+        </div>
+        <div className='grid-7 gutter-1'>
+          <div className='card'>
+            <p>1</p>
+          </div>
+          <div className='card'>
+            <p>2</p>
+          </div>
+          <div className='card'>
+            <p>3</p>
+          </div>
+          <div className='card'>
+            <p>4</p>
+          </div>
+          <div className='card'>
+            <p>5</p>
+          </div>
+          <div className='card'>
+            <p>6</p>
+          </div>
+          <div className='card'>
+            <p>7</p>
+          </div>
+        </div>
+        <div className='grid-8 gutter-1'>
+          <div className='card'>
+            <p>1</p>
+          </div>
+          <div className='card'>
+            <p>2</p>
+          </div>
+          <div className='card'>
+            <p>3</p>
+          </div>
+          <div className='card'>
+            <p>4</p>
+          </div>
+          <div className='card'>
+            <p>5</p>
+          </div>
+          <div className='card'>
+            <p>6</p>
+          </div>
+          <div className='card'>
+            <p>7</p>
+          </div>
+          <div className='card'>
+            <p>8</p>
+          </div>
+        </div>
+        <div className='grid-9 gutter-1'>
+          <div className='card'>
+            <p>1</p>
+          </div>
+          <div className='card'>
+            <p>2</p>
+          </div>
+          <div className='card'>
+            <p>3</p>
+          </div>
+          <div className='card'>
+            <p>4</p>
+          </div>
+          <div className='card'>
+            <p>5</p>
+          </div>
+          <div className='card'>
+            <p>6</p>
+          </div>
+          <div className='card'>
+            <p>7</p>
+          </div>
+          <div className='card'>
+            <p>8</p>
+          </div>
+          <div className='card'>
+            <p>9</p>
+          </div>
+        </div>
+        <div className='grid-10 gutter-1'>
+          <div className='card'>
+            <p>1</p>
+          </div>
+          <div className='card'>
+            <p>2</p>
+          </div>
+          <div className='card'>
+            <p>3</p>
+          </div>
+          <div className='card'>
+            <p>4</p>
+          </div>
+          <div className='card'>
+            <p>5</p>
+          </div>
+          <div className='card'>
+            <p>6</p>
+          </div>
+          <div className='card'>
+            <p>7</p>
+          </div>
+          <div className='card'>
+            <p>8</p>
+          </div>
+          <div className='card'>
+            <p>9</p>
+          </div>
+          <div className='card'>
+            <p>10</p>
+          </div>
+        </div>
+        <div className='grid-11 gutter-1'>
+          <div className='card'>
+            <p>1</p>
+          </div>
+          <div className='card'>
+            <p>2</p>
+          </div>
+          <div className='card'>
+            <p>3</p>
+          </div>
+          <div className='card'>
+            <p>4</p>
+          </div>
+          <div className='card'>
+            <p>5</p>
+          </div>
+          <div className='card'>
+            <p>6</p>
+          </div>
+          <div className='card'>
+            <p>7</p>
+          </div>
+          <div className='card'>
+            <p>8</p>
+          </div>
+          <div className='card'>
+            <p>9</p>
+          </div>
+          <div className='card'>
+            <p>10</p>
+          </div>
+          <div className='card'>
+            <p>11</p>
+          </div>
+        </div>
+        <div className='grid-12 gutter-1'>
+          <div className='card'>
+            <p>1</p>
+          </div>
+          <div className='card'>
+            <p>2</p>
+          </div>
+          <div className='card'>
+            <p>3</p>
+          </div>
+          <div className='card'>
+            <p>4</p>
+          </div>
+          <div className='card'>
+            <p>5</p>
+          </div>
+          <div className='card'>
+            <p>6</p>
+          </div>
+          <div className='card'>
+            <p>7</p>
+          </div>
+          <div className='card'>
+            <p>8</p>
+          </div>
+          <div className='card'>
+            <p>9</p>
+          </div>
+          <div className='card'>
+            <p>10</p>
+          </div>
+          <div className='card'>
+            <p>11</p>
+          </div>
+          <div className='card'>
+            <p>12</p>
+          </div>
+        </div>
+
       </div>
     );
   }
